a857c7c26e9f459ef9c0882557f8960179879469
jubrealguy <whizkidjubrealguy@gmail.com>
2024-02-07 16:44:58 +0100

    re-organization of files

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,6 @@
-import './App.css';
-import logo from './holberton-logo.jpg'
-import { getFullYear, getFooterCopy } from './utils';
+import './App.css'
+import logo from '../assets/holberton-logo.jpg'
+import { getFullYear, getFooterCopy } from '../utils/utils';
 
 function App() {
   return (
